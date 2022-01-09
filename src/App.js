701aee8f60670db6a1ce02e637fb63c892b058fb
Stas Mirko <stasmq@gmail.com>
2022-01-09 09:51:57 +0200

    Started project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+        <header className='header'>
+            <img src = 'https://img.favpng.com/23/20/25/lay-s-logo-potato-chip-frito-lay-png-favpng-BwHwmxmXDmn6wyw2MR2Tncsic.jpg'/>
+        </header>
+        <nav className='nav'>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Video</a></div>
+            <div><a>Settings</a></div>
+        </nav>
+        <div className='content'>
+            <div>
+                <img src='https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80'/>
+            </div>
+            <div>AVA + Descr</div>
+            <div>
+                <div>New posts</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+                <div>Post 3</div>
+            </div>
+        </div>
     </div>
   );
 }
