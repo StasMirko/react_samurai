17cfaea88ce43d53186b940c8e006122a57f5c9e
StasMirko <stasmq@gmail.com>
2022-03-19 10:50:24 +0200

    css modules 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import s from './Navbar.module.css';
 const NavBar = () => {
     return (
         <nav className={ s.nav }>
-            <div className={ s.item }><a>Messages</a></div>
+            <div className={ s.item }><a>Messages 1</a></div>
             <div className={`${s.item} ${s.active}`}><a>News</a></div>
             <div className={ s.item }><a>Music</a></div>
             <div className={ s.item }><a>Video</a></div>
